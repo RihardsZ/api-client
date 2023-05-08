50d53927de81ee820f250b2774422a6bb5d7ea9d
Rihards Zbitkovskis <rihards.zbitkovskis@cubesystems.lv>
2023-05-08 03:07:00 +0300

    Add boolean string constants to AbstractPayload

--- src/Client/Payloads/AbstractPayload.php
+++ src/Client/Payloads/AbstractPayload.php
@@ -6,5 +6,6 @@ use CubeSystems\SoapClient\Client\Contracts\Payload;
 
 abstract class AbstractPayload implements Payload
 {
-
+    protected const STRING_BOOL_TRUE = 'Y';
+    protected const STRING_BOOL_FALSE = 'N';
 }
