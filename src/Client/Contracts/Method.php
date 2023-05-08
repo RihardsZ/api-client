8a39f4c9541151437cf66eeda545154a9692277e
Rihards Zbitkovskis <rihards.zbitkovskis@cubesystems.lv>
2023-05-08 03:15:49 +0300

    Add getName and getService to Method contract; emit events on service calling and cache retrieval

--- src/Client/Contracts/Method.php
+++ src/Client/Contracts/Method.php
@@ -5,4 +5,8 @@ namespace CubeSystems\SoapClient\Client\Contracts;
 interface Method
 {
     public function call(Payload $payload): Response;
+
+    public function getName(): string;
+
+    public function getService(): Service;
 }
--- src/Client/Methods/AbstractMethod.php
+++ src/Client/Methods/AbstractMethod.php
@@ -2,18 +2,22 @@
 
 namespace CubeSystems\SoapClient\Client\Methods;
 
+use CodeDredd\Soap\Client\Response as RawResponse;
+
 use CubeSystems\SoapClient\Client\Contracts\CacheStrategy;
+use CubeSystems\SoapClient\Client\Contracts\Method;
 use CubeSystems\SoapClient\Client\Contracts\Payload;
 use CubeSystems\SoapClient\Client\Contracts\Response;
 use CubeSystems\SoapClient\Client\Contracts\Service;
+use CubeSystems\SoapClient\Client\Stats\CallStats;
+use CubeSystems\SoapClient\Events\ResponseRetrievedFromCache;
+use CubeSystems\SoapClient\Events\ServiceCalled;
 
-abstract class AbstractMethod
+abstract class AbstractMethod implements Method
 {
     private Service $service;
 
     private CacheStrategy $cacheStrategy;
-
-    protected const METHOD_NAME = '';
 
     public function __construct(Service $service, CacheStrategy $cacheStrategy)
     {
@@ -23,22 +27,94 @@ abstract class AbstractMethod
 
     public function call(Payload $payload): Response
     {
-        return $this->cacheStrategy->cache($payload->getCacheKey(), function () use ($payload) {
-            $rawResult = $this->service->getClient()->call(
-                $this->getName(),
-                $payload->toArray()
-            );
+        $cachePresent = $this->cacheStrategy->getCache()->has($payload->getCacheKey());
 
-            $associativeArray = $rawResult->json();
+        if ($cachePresent) {
+            return $this->retrieveFromCache($payload);
+        }
 
-            return $this->toResponse($associativeArray);
-        });
+        return $this->retrieveFromRemote($payload);
     }
 
-    private function getName(): string
+    public function getName(): string
     {
         return static::METHOD_NAME;
     }
 
+    public function getService(): Service
+    {
+        return $this->service;
+    }
+
     abstract protected function toResponse(array $rawResponse): Response;
+
+    private function retrieveFromCache(Payload $payload)
+    {
+        $response = $this->cacheStrategy->getCache()->get($payload->getCacheKey());
+
+        $stats = new CallStats();
+        $stats->setMicrotimeStart(microtime(true));
+
+        ResponseRetrievedFromCache::dispatch(
+            $this,
+            $payload,
+            $response,
+            $stats
+        );
+
+        return $response;
+    }
+
+    private function retrieveFromRemote(Payload $payload): Response
+    {
+        return $this->cacheStrategy->cache($payload->getCacheKey(), function () use ($payload) {
+            $microtimeFrom = microtime(true);
+
+            $rawResponse = $this->service->getClient()->call(
+                $this->getName(),
+                $payload->toArray()
+            );
+
+            $microtimeTo = microtime(true);
+
+            $response = $this->toResponse($rawResponse->json());
+
+            $this->dispatchServiceCalledEvent(
+                $payload,
+                $rawResponse,
+                $response,
+                $microtimeFrom,
+                $microtimeTo
+            );
+
+            return $response;
+        });
+    }
+
+    private function dispatchServiceCalledEvent(
+        Payload $payload,
+        RawResponse $rawResponse,
+        Response $response,
+        float $microtimeFrom,
+        float $microtimeTo
+    ): void {
+        $debugInfo = $this->service->getClient()->debugLastSoapRequest();
+
+        $stats = new CallStats();
+        $stats
+            ->setRequestString($debugInfo['request']['body'])
+            ->setRequestHeaders(collect($rawResponse->transferStats->getRequest()->getHeaders()))
+            ->setResponseString($debugInfo['response']['body'])
+            ->setResponseHeaders(collect($rawResponse->transferStats->getResponse()->getHeaders()))
+            ->setMicrotimeStart($microtimeFrom)
+            ->setMicrotimeFinish($microtimeTo)
+            ->setTransferStats($rawResponse->transferStats);
+
+        ServiceCalled::dispatch(
+            $this,
+            $payload,
+            $response,
+            $stats
+        );
+    }
 }
